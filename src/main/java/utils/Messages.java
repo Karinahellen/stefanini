4ee7368ff459ecb5393b1e2c5dafdf8e0ffa034f
fsw.karina.hellen <$noc1109>
2020-09-30 22:25:00 -0300

    Removendo arquivos desnecessarios

--- src/main/java/utils/Messages.java
+++ src/main/java/utils/Messages.java
@@ -5,7 +5,5 @@ package utils;
  */
 public class Messages {
 
-    public static final String MSG_INCLUSAO_SUCESSO = "Dados incluídos com sucesso!";
 
-    public static final String MSG_EXCECAO_LABEL_INEXISTENTE = "Não foi encontrada a label: ";
 }
